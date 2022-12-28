c71a518f9d143a1c7354f1beab4d100e567f07e1
Hem <hemk2810@gmail.com>
2022-12-28 14:28:54 +0530

    ready textutils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -51,17 +51,17 @@ export default function TextForm(props) {
         <div className={`container my-4 text-${props.mode === 'light'?'black':'white'}`}>
         <h1>Enter Text for Analyze:</h1>
         <textarea className="form-control" id='tb' value={text} style={{backgroundColor : props.mode === 'light'?'#CFECEC':'#15317E' , color :props.mode === 'light'?'black':'white'}} onChange={handleOnChange} rows ="8"></textarea>
-        <button className="btn btn-warning mx-3 my-4" onClick={handleUpClick}>Convert to UpperCase</button>
-        <button className="btn btn-warning mx-3" onClick={handleLowClick}>Convert to LowerCase</button>
-        <button className="btn btn-warning mx-3" onClick={handleClClick}>Clear Text</button>
-        <button className="btn btn-warning mx-3" onClick={handleCopy}>Copy Text</button>
-        <button className="btn btn-warning mx-2" onClick={handlExSpace}>Remove Extra Space</button>
+        <button hidden={text.length===0} className="btn btn-warning mx-3 my-3" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button hidden={text.length===0} className="btn btn-warning mx-3 my-3" onClick={handleLowClick}>Convert to LowerCase</button>
+        <button hidden={text.length===0} className="btn btn-warning mx-3 my-3" onClick={handleClClick}>Clear Text</button>
+        <button hidden={text.length===0} className="btn btn-warning mx-3 my-3" onClick={handleCopy}>Copy Text</button>
+        <button hidden={text.length===0} className="btn btn-warning mx-2 my-3" onClick={handlExSpace}>Remove Extra Space</button>
         </div>
 
         <div className={`container my-3 text-${props.mode === 'light'?'black':'white'}`}>
           <h2>Entered Text Summary:</h2>
             <p>{text.length} Characters & {text.split(" ").filter((ele)=>{return ele.length!==0}).length} Words</p>
-            <p>It will take {0.008 *  text.split(" ").length} Minutes to read </p>
+            <p hidden={text.length===0}>It will take {0.008 *  text.split(" ").length} Minutes to read </p>
             <h3>Preview Of Entered Text -</h3>
             <p>{text.length>0?text:"Enter text in textbox to preview it here"}</p>
 
